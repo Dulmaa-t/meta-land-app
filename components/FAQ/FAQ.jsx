fa58b416c56b80fa6c4267e2676a790bc51ed1f5
Dulmaa-t <duka9705@gmail.com>
2023-02-03 14:58:01 +0800

    faq

--- components/FAQ/FAQ.jsx
+++ components/FAQ/FAQ.jsx
@@ -1,71 +1,91 @@
-
-import { BiPlus } from "react-icons/bi";
+import { useEffect, useState } from "react";
+import { BiPlus, BiMinus } from "react-icons/bi";
 
 const FAQ = () => {
-    return (
-        <div className="flex flex-row text-[24px] gap-16 mt-10">
-            <div className="flex flex-col leading-loose ">
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center justify-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">When the musics over turn off the light?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">What is the best way to collect NFT?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">Why NFT Trading is so popular?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">How do you trade on Montono easily?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-            </div>
-            <div className="flex flex-col leading-loose ">
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">When the musics over turn off the light?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">What is the best way to collect NFT?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">Why NFT Trading is so popular?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-                <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
-                    <div className="flex flex-row items-center">
-                        <BiPlus className="mx-6" />
-                        <h2 id="heading-1">How do you trade on Montono easily?</h2>
-                    </div>
-                    <div id="body-1" className="hidden" aria-labelledby="heading-1">cc</div>
-                </div>
-            </div>
+    const [isOpen, setIsOpen] = useState(false);
 
+    return (
+        <div class="mx-auto mt-8 text-oscuro">
+            <div class="flex flex-row justify-center w-full">
+                <div className="mr-10">
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle1" class="toggle hidden" />
+                        <label class="title block font-bold  cursor-pointer" for="toggle1">
+                            When the musics over turn off the light?
+                        </label>
+                        <div class="content overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle2" class="toggle hidden" />
+                        <label class="title block font-bol  cursor-pointer" for="toggle2">
+                            What is the best way to collect NFT?
+                        </label>
+                        <div class="content  overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle3" class="toggle hidden" />
+                        <label class="title block font-bold  cursor-pointer" for="toggle3">
+                            Why NFT Trading is so popular?
+                        </label>
+                        <div class="content overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle3" class="toggle hidden" />
+                        <label class="title block font-bold  cursor-pointer" for="toggle3">
+                            How do you trade on Montono easily?
+                        </label>
+                        <div class="content overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle1" class="toggle hidden" />
+                        <label class="title block font-bold  cursor-pointer" for="toggle1">
+                            When the musics over turn off the light?
+                        </label>
+                        <div class="content overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle3" class="toggle hidden" />
+                        <label class="title block font-bold  cursor-pointer" for="toggle3">
+                            What is the best way to collect NFT?
+                        </label>
+                        <div class="content overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle2" class="toggle hidden" />
+                        <label class="title block font-bol  cursor-pointer" for="toggle2">
+                            Why NFT Trading is so popular?
+                        </label>
+                        <div class="content  overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                    <div class="relative mb-4">
+                        <input type="checkbox" id="toggle3" class="toggle hidden" />
+                        <label class="title block font-bold  cursor-pointer" for="toggle3">
+                            How do you trade on Montono easily?
+                        </label>
+                        <div class="content overflow-hidden">
+                            <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
--- pages/collections.jsx
+++ pages/collections.jsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import SubBanner from "../components/SubBanner/SubBanner";
+import item26 from "../public/item-26.png";
 
 const Collections = () => {
     return (
         <div className="container mx-auto">
             <SubBanner />
+            <div className="flex flex-row my-20">
+                <div className="flex">
+                    <Image src={item26} alt="item26" />
+                </div>
+                <div className="flex-1 px-20" data-aos="fade-up">
+                    <h1>About Monteno ii</h1>
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occae cat cupidatat non proident, sunt in culpa</p>
+                    <h2>Features</h2>
+                    <h2>Likes</h2>
+                </div>
+            </div>
         </div>
     )
 }
